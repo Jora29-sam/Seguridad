237014d9a7dab94db0744083a5f2022bf36d42b0
Leon <leon05032003@gmail.com>
2026-05-22 14:02:09 -0600

    area de triángulo

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('quadratic-calculator');
 });
+
+use App\Http\Controllers\OperationsController;
+
+Route::get('/triangle-area', function () {
+
+    $result = null;
+
+    if (request()->has('base') && request()->has('height')) {
+
+        $controller = new OperationsController;
+
+        $result = $controller->triangleArea(
+            (float) request('base'),
+            (float) request('height')
+        );
+    }
+
+    return view('triangle-area', compact('result'));
+});
